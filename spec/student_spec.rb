caaf7f78547386801f958402904f462f42ca2de3
KaraJH <karas.jones@yahoo.com>
2022-10-27 09:49:05 -0600

    feat: Creates passing test for student object and name

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,4 +1,28 @@
 require './lib/student'
 
 RSpec.describe Student do
+  it 'checks stat student exists' do
+  student = Student.new({name: "Morgan", age: 21})
+
+  expect(student).to be_a(Student)
+  end
+  it ' checks the name of the student' do
+    student = Student.new({name: "Morgan", age: 21})
+
+    expect(student.name).to eq("Morgan")
+  end
+  it ' has an empty array for scores' do
+    student = Student.new({name: "Morgan", age: 21})
+
+    expect(student.scores).to eq([])
+  end
+
+  describe '#log_score' do
+    it ' takes an argument for score and puts it into the score array' do
+      student = Student.new({name: "Morgan", age: 21})
+      student.log_score(89)
+      student.log_score(78)
+
+    end
+  end
 end
